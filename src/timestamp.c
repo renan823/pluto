5c1b7b05e398db33a745123cb410a1794e17b04d
renan823 <renan.trofino@gmail.com>
2025-09-21 13:01:48 -0300

    feat: timestamp impl

---
+++ src/timestamp.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <sys/time.h>
+
+#include "timestamp.h"
+
+/*
+Returns the current time in milliseconds.
+*/
+Timestamp CurrentTimestamp(void) {
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+
+    return (int64_t)tv.tv_sec * 1000 + tv.tv_usec / 1000;
+}
+
+/*
+Parses the given string date into a valid timestamp.
+*/
+Timestamp ParseTimestamp(char *timestamp) {
+    return -1;
+}
